d5ec84c654d66b4d8b2b98107d3e2b8f1aace6ce
ElNuevoAprendiz <profedanieljuan@gmail.com>
2024-10-31 20:44:36 -0300

    Se crea un formulario en donde se utiliza JTextFlield

---
+++ FormularioConJTextField.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class FormularioConJTextField extends JFrame implements ActionListener{
+    private JButton boton;
+    private JTextField texto;
+    private JLabel etiqueta;
+
+    public FormularioConJTextField(){
+        setLayout(null);
+
+        boton = new JButton("Aceptar");
+        boton.setBounds(10,80,100,30);
+        add(boton);
+        boton.addActionListener;
+
+        etiqueta = JLabel("Usuario");
+        etiqueta.setBounds(10,10,100,30);
+        add(etiqueta);
+
+        texto = new JTextField();
+        texto.setBounds(120,10,150,20);
+        add(texto);
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if (e.getSource==boton){
+            setTitle(texto.getText());
+        }
+    }
+
+    public static void main(String[] args){
+        FormularioConJTextField formulario = new FormularioConJTextField();
+        formulario.setBounds(0,0,350,170);
+        formulario.setVisible(true);
+        formulario.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+}
